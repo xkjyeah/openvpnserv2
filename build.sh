d8b092e0a1127330343dea52f996013575340692
Daniel Sim <daniel.ssq89@gmail.com>
2016-11-21 23:19:36 +0800

    exit the build early if there's an error

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 xbuild /p:Configuration=Release "/p:Platform=Any CPU" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
 xbuild /p:Configuration=Release "/p:Platform=x86" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
 xbuild /p:Configuration=Release "/p:Platform=x64" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
 
 xbuild /p:Configuration=Debug "/p:Platform=Any CPU" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
 xbuild /p:Configuration=Debug "/p:Platform=x86" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
 xbuild /p:Configuration=Debug "/p:Platform=x64" OpenVpnService.sln 
+if [ "$?" != "0" ]; then exit 1; fi
